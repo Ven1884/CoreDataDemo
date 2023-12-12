47ff7d5e1bf866a6f72ac234368b4ef449aff6c1
Ven1456 <venkatabmw@gmail.com>
2023-12-12 13:17:24 +0530

    - perform the CRUD Operation - Dispay the Data into Table View Cell's

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -13,38 +13,37 @@ struct UserModel{
     var name:String
 }
 
+enum AlertMode{
+    case add
+    case update(email:String,name:String,index:Int)
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var tblOutlet: UITableView!
     var userArr = [UserModel]()
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchAllRecords()
-
     }
     @IBAction func addUserBtnClicked(_ sender: UIBarButtonItem) {
-         showTwoTextFieldAlert()
+        showAlert(mode: .add)
     }
 }
-
-// Perform CRUD
+//MARK: - Perform CRUD
 extension ViewController{
-    // CREATE RECORDS
+    //MARK: - CREATE RECORDS
     func createUserRecords(userEmail:String,userName:String){
         //1 create appdelegate Singleton object
         guard  let appdelegate = UIApplication.shared.delegate as? AppDelegate else {return }
-        
         //2.Access persistentContainer from appdelegate Singleton object and Access the singleton managed object context
         let viewContext = appdelegate.persistentContainer.viewContext
-        
         //3. Create an entity
-        
         if let userEntity = NSEntityDescription.entity(forEntityName: "Users", in: viewContext){
             //4. Create managed object
             let stud = NSManagedObject(entity: userEntity, insertInto: viewContext)
             stud.setValue(userEmail, forKey: "email")
             stud.setValue(userName, forKey: "name")
         }
-        
         //5. Save to persistent store
         if viewContext.hasChanges{
             do{
@@ -56,8 +55,8 @@ extension ViewController{
         }
         
     }
-
-    // FETCH RECORDS
+    
+    //MARK: - FETCH RECORDS
     func fetchAllRecords(){
         //1 create appdelegate Singleton object
         guard  let appdelegate = UIApplication.shared.delegate as? AppDelegate else {return }
@@ -79,35 +78,32 @@ extension ViewController{
             print("unable to fetch the data")
         }
     }
-    //UPDATE RECORDS
-    func updateRecords() {
+    //MARK: - UPDATE RECORDS
+    func updateRecords(name:String,newName:String,email:String,newEmail:String) {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         let viewContext = appDelegate.persistentContainer.viewContext
-        
         // Prepare the request of type NSFetchRequest for the entity
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
-        
         // Add a predicate to fetch specific records you want to update
-        fetchRequest.predicate = NSPredicate(format: "name == %@", "Venkat")
-        
+        fetchRequest.predicate = NSPredicate(format: "name == %@", name)
+        fetchRequest.predicate = NSPredicate(format: "email == %@", email)
         do {
             // Fetch the records based on the fetch request
             let fetchedRecords = try viewContext.fetch(fetchRequest) as! [NSManagedObject]
-            
             // Update fetched records
             for record in fetchedRecords {
-                record.setValue("Hari", forKey: "name")
+                record.setValue(newName, forKey: "name")
+                record.setValue(newEmail, forKey: "email")
             }
-            
             // Save the context to persist changes
             try viewContext.save()
-            fetchAllRecords()
+            tblOutlet.reloadData()
             print("Records updated successfully")
         } catch let error as NSError {
             print("Could not update records. \(error), \(error.userInfo)")
         }
     }
-    //DELETE RECORDS
+    //MARK: - DELETE RECORDS
     func deleteRecords(name:String) {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         let viewContext = appDelegate.persistentContainer.viewContext
@@ -135,73 +131,74 @@ extension ViewController{
             print("Could not delete records. \(error), \(error.userInfo)")
         }
     }
-    
-    func showTwoTextFieldAlert() {
-        let alertController = UIAlertController(title: "Add User Details", message: nil, preferredStyle: .alert)
-        
-        alertController.addTextField { (textField1) in
-            textField1.placeholder = "Enter User Email"
+    //MARK: - SHOW ALERT
+    func showAlert(mode:AlertMode){
+        let alertController:UIAlertController
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let submitTitle:String
+        // TEXTFIELD'S
+        switch mode {
+        case .add:
+            alertController = UIAlertController(title: "Add User Details", message: nil, preferredStyle: .alert)
+            alertController.addTextField { (tf1) in
+                tf1.placeholder = "Enter User Email"
+            }
+            alertController.addTextField { (tf2) in
+                tf2.placeholder = "Enter User Name"
+            }
+            submitTitle = "Submit"
+        case let .update(email: email, name: name, _):
+            alertController = UIAlertController(title: "Update User Details", message: nil, preferredStyle: .alert)
+            alertController.addTextField { (tf1) in
+                tf1.text = email
+            }
+            alertController.addTextField { (tf2) in
+                tf2.text = name
+            }
+            submitTitle = "Update"
         }
-        
-        alertController.addTextField { (textField2) in
-            textField2.placeholder = "Enter User Name"
-        }
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        
-        let submitAction = UIAlertAction(title: "Submit", style: .default) { (action) in
+        let submitAction = UIAlertAction(title: submitTitle, style: .default) { [weak self] (action) in
+            guard let weakself = self,let textFields = alertController.textFields else {return}
             
-            // Handle the user's input when they tap "Submit"
-            if let textFields = alertController.textFields {
-                let firstTextField = textFields[0]
-                let secondTextField = textFields[1]
+            switch mode {
+            case .add:
+                if let firstText = textFields[0].text, !firstText.isEmpty,
+                   let secondText = textFields[1].text, !secondText.isEmpty {
+                    // Text fields are not empty, proceed with user creation/update
+                    weakself.createUserRecords(userEmail: firstText, userName: secondText)
+                    weakself.userArr.append(UserModel(email: firstText, name: secondText))
+                    weakself.tblOutlet.reloadData()
+                } else {
+                    // Show an error message as fields are empty
+                    weakself.showErrorMessage()
+                }
                 
-                if let firstText = firstTextField.text, let secondText = secondTextField.text {
-                    self.createUserRecords(userEmail: firstText, userName: secondText)
-                    self.userArr.append(UserModel(email: firstText, name: secondText))
-                    self.tblOutlet.reloadData()
+            case .update(let email,let name, let index):
+                if let firstText = textFields[0].text, !firstText.isEmpty,
+                   let secondText = textFields[1].text, !secondText.isEmpty {
+                    // Update the userArr with the new details at the specified index
+                    weakself.userArr[index] = UserModel(email: firstText, name: secondText)
+                    weakself.updateRecords(name: name, newName: secondText,email: email,newEmail: firstText)
+                } else {
+                    // Show an error message as fields are empty
+                    weakself.showErrorMessage()
                 }
             }
         }
-        
-        alertController.addAction(cancelAction)
+        alertController.addAction(cancel)
         alertController.addAction(submitAction)
-        
         present(alertController, animated: true, completion: nil)
+    }
+    //ERROR ALERT
+    func showErrorMessage() {
+        let errorAlert = UIAlertController(title: "Error", message: "Text fields cannot be empty", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        errorAlert.addAction(okAction)
+        present(errorAlert, animated: true, completion: nil)
     }
     
-   
-    func updateTextFieldAlert(email: String, name: String, index: Int) {
-        let alertController = UIAlertController(title: "Update User Details", message: nil, preferredStyle: .alert)
-        alertController.addTextField { (textField1) in
-            textField1.text = email
-        }
-        
-        alertController.addTextField { (textField2) in
-            textField2.text = name
-        }
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        
-        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self] (action) in
-            // Handle the user's input when they tap "Submit"
-            if let textFields = alertController.textFields,
-               let firstTextField = textFields[0].text,
-               let secondTextField = textFields[1].text,
-               let weakSelf = self {
-                // Update the userArr with the new details at the specified index
-                weakSelf.userArr[index] = UserModel(email: firstTextField, name: secondTextField)
-                weakSelf.tblOutlet.reloadData()
-            }
-        }
-        
-        alertController.addAction(cancelAction)
-        alertController.addAction(submitAction)
-        
-        present(alertController, animated: true, completion: nil)
-    }
 }
-
+//MARK: - TABLE VIEW EXTENSION
 extension ViewController:UITableViewDataSource,UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return userArr.count
@@ -215,20 +212,39 @@ extension ViewController:UITableViewDataSource,UITableViewDelegate{
         tblOutlet.estimatedRowHeight = 68.0
         return cell
     }
-    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
+    func tableView(_ tableView: UITableView,
+                   editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
     }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let user = userArr[indexPath.row]
-        updateTextFieldAlert(email: user.email, name: user.name, index: indexPath.row)
-        print(user.name)
-    }
-     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-         if editingStyle == .delete {
-             let user = userArr[indexPath.row]
-             deleteRecords(name: user.name)
-             userArr.remove(at: indexPath.row)
-             tableView.deleteRows(at: [indexPath], with: .fade)
-         }
+    //MARK: - SWIPE TO EDIT AND DELETE FUNCTIONALITY
+    func tableView(_ tableView: UITableView,
+                   trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        // Archive action
+        let edit = UIContextualAction(style: .normal,
+                                      title: "Edit") { [weak self] (action, view, completionHandler) in
+            if let user = self?.userArr[indexPath.row]{
+                
+                self?.showAlert(mode: .update(email: user.email, name: user.name, index: indexPath.row))
+                
+            }
+            completionHandler(true)
+        }
+        edit.backgroundColor = .systemGreen
+        
+        // Trash action
+        let delete = UIContextualAction(style: .destructive,
+                                        title: "Delete") { [weak self] (action, view, completionHandler) in
+            guard let self = self else { return } //
+            let user = userArr[indexPath.row]
+            deleteRecords(name: user.name)
+            userArr.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            completionHandler(true)
+        }
+        delete.backgroundColor = .systemRed
+        
+        let configuration = UISwipeActionsConfiguration(actions: [edit, delete])
+        
+        return configuration
     }
 }
